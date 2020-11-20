9c876907a691fff69499aee6eaeca9dfa3456146
Philippe De Pauw - Waterschoot <drdynscript>
2020-11-20 13:41:53 +0100

    Added examples oop for 1PGMa

--- 1pgm-a/week-1/college/oop.js
+++ 1pgm-a/week-1/college/oop.js
@@ -38,6 +38,18 @@ function TagCloud (id, name) {
     this.addTag = function(word) {
         this.tags.push(word);
     };
+    this.toString = function () {
+        return `${this.name}`;
+    };
+    this.toCLIString = function () {
+        return `
+=================================================
+${this.name}
+-------------------------------------------------
+${this.tags.map((w) => w).join('\n')}
+=================================================
+        `;
+    }
 };
 
 // Make an instance of a tag cloud
@@ -47,3 +59,50 @@ tc1.addTag('Ruby');
 tc1.addTag('Rust');
 tc1.addTag('Python');
 console.log(tc1);
+console.log(tc1.toString());
+console.log(tc1.toCLIString());
+
+// Describe a category
+function Category (name, description = null) {
+    this.name = name;
+    this.description = description;
+};
+
+// Describe a post
+function Post ({ title, subTitle = null, synopsis, body, category = null }) {
+    this.title = title;
+    this.subTitle = subTitle;
+    this.synopsis = synopsis;
+    this.body = body;
+    this.category = category;
+    this.toCLIString = function () {
+        return `
+=================================================
+${this.title}
+-------------------------------------------------
+Category: ${this.category !== null ? this.category.name : 'Uncategorized'}
+-------------------------------------------------
+${this.synopsis}
+-------------------------------------------------
+=================================================
+        `;
+    };
+};
+
+// Make an instance of the class Category
+const cat1 = new Category('Reviews', 'Tweakers biedt de meeste en uitgebreidste professionele- en gebruikersreviews over computers, tablets & telefoons, beeld & geluid, games, IT Pro en alle andere consumentenelektronica.');
+console.log(cat1);
+
+const cat2 = new Category('Pricewatch', 'Vergelijk prijzen van o.a. smartphones, laptops, tablets, televisies en camera\'s. Lees uitgebreide reviews en maak de beste keuze.');
+console.log(cat2);
+
+// Make an instance of the class Post
+const post1 = new Post({
+    subTitle: 'Een goedkoop en aantrekkelijk alternatief',
+    title: 'Xbox Series S Review',
+    body: 'Ipsum',
+});
+post1.synopsis = 'Microsoft plaatst een hele slimme zet met de Xbox Series S. Het is een mooie en lekker kleine console die toch met veel moderne technieken overweg kan. Gamen op 4k zit er niet in, dat is de voornaamste concessie die je moet doen. Maar als je geen 4k-tv hebt, is de Series S een prima alternatief voor de duurdere Series X. De Series S is daarmee een ideale instapmachine die vooral door de aantrekkelijke Game Pass veel mensen voor het Microsoft-kamp zal winnen.';
+post1.category = cat1;
+console.log(post1);
+console.log(post1.toCLIString());
