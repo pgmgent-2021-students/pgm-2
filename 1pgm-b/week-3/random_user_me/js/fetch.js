60947f79ee978369efcfa62c2c1e2c8134cc815d
Philippe De Pauw - Waterschoot <drdynscript>
2020-12-03 14:47:20 +0100

    Added RandomUser Me

--- 1pgm-b/week-3/random_user_me/js/fetch.js
+++ 1pgm-b/week-3/random_user_me/js/fetch.js
@@ -1,15 +1,18 @@
-function getJSON(url, successHandler, errorHandler) {
+function getJSON(url, successHandler, errorHandler){
+    // eslint-disable-next-line no-undef
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
     xhr.responseType = 'json';
+    xhr.open('GET', url, true);
     xhr.onload = () => {
-        if (xhr.status === 200) {
-            const data = (!xhr.responseType) ? JSON.parse(xhr.response) : xhr.response;
-
-            successHandler && successHandler(data);
-        } else {
-            errorHandler && errorHandler('Something went wrong!');
-        }
-    }
+      if (xhr.status === 200) {
+        const data = (!xhr.responseType) ? JSON.parse(xhr.response) : xhr.response;
+        successHandler && successHandler(data);
+      } else {
+        errorHandler && errorHandler(xhr.status);
+      }
+    };
+    xhr.onerror = () => {
+      errorHandler && errorHandler('Network Error!');
+    };
     xhr.send(null);
 }
