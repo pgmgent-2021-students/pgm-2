e97064f2a204660150e3690465e0b61571150f9b
Thibault Fouquaert <thibfo@arteveldehs.be>
2020-12-10 13:05:52 +0100

    week 4 b

--- 1pgm-b/week-4/server/expressServer.js
+++ 1pgm-b/week-4/server/expressServer.js
@@ -1,8 +1,14 @@
-const express = require('exress')
+const express = require('express')
 const app = express()
+
+const hostname = '127.0.0.1';
+const port = process.env.PORT || 8080;
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World changes')
 })
+
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  })
  
-app.listen(3000)
